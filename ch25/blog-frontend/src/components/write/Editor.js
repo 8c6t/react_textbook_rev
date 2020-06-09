01f2f4490a5d2ada42507ec4c6d15a4258a82192
8c6t <nkw8620@gmail.com>
2020-06-09 22:03:15 +0900

    ch25. 프런트엔드 프로젝트: 글쓰기 기능 구현하기

---
+++ ch25/blog-frontend/src/components/write/Editor.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useRef } from 'react';
+import Quill from 'quill';
+import 'quill/dist/quill.bubble.css';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+import Responsive from '../common/Responsive';
+
+const EditorBlock = styled(Responsive)`
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+`;
+
+const TitleInput = styled.input`
+  font-size: 3rem;
+  outline: none;
+  padding-bottom: 0.5rem;
+  border: none;
+  border-bottom: 1px solid ${palette.gray[4]};
+`;
+
+const QuillWrapper = styled.div`
+  .ql-editor {
+    padding: 0;
+    min-height: 320px;
+    font-size: 1.125rem;
+    line-height: 1.5;
+  }
+
+  .ql-editor.ql-blnak::before {
+    left: 0px;
+  }
+`;
+
+const Editor = ({ title, body, onChangeField }) => {
+  const quillElement = useRef(null);
+  const quillInstance = useRef(null);
+
+  useEffect(() => {
+    quillInstance.current = new Quill(quillElement.current, {
+      theme: 'bubble',
+      placeholder: '내용을 작성하세요...',
+      modules: {
+        toolbar: [
+          [{ header: '1' }, { header: '2' }],
+          ['bold', 'italic', 'underline', 'strike'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          ['blockquote', 'code-block', 'link', 'image'],
+        ],
+      },
+    });
+
+    const quill = quillInstance.current;
+    quill.on('text-change', (delta, oldDelta, source) => {
+      if (source === 'user') {
+        onChangeField({ key: 'body', value: quill.root.innerHTML });
+      }
+    });
+  }, [onChangeField]);
+
+  const onCHangeTitle = (e) => {
+    onChangeField({ key: 'title', value: e.target.value });
+  };
+
+  return (
+    <EditorBlock>
+      <TitleInput
+        placeholder="제목을 입력하세요"
+        onChange={onCHangeTitle}
+        value={title}
+      />
+      <QuillWrapper>
+        <div ref={quillElement} />
+      </QuillWrapper>
+    </EditorBlock>
+  );
+};
+
+export default Editor;
